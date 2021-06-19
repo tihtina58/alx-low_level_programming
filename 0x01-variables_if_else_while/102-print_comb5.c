ed29758a7233b24137fed8a488ab4b8fdb44fb4b
tihitna <tihitnasete58@gmail.com>
2021-06-18 23:22:08 -0700

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - 2 digits
+ *main - print two two digit numbers 
  *
- *return: 0
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,10 +12,14 @@ for (i = '0'; i <= '9'; i++)
 {
 for (j = '0'; j <= '9'; j++)
 {
-for (k = i; k <= '9'; k++)
+for (k = '0'; k <= '9'; k++)
 {
-for (l = j + 1; l <= '9'; l++)
+for (l = '0'; l <= '9'; l++)
 {
+if (j > l)
+{
+continue;
+}
 putchar(i);
 putchar(j);
 putchar(' ');
@@ -25,6 +29,7 @@ if (i != '9' || j != '8' || k != '9' || l != '9')
 {
 putchar(',');
 putchar(' ');
+break;
 }
 }
 }
