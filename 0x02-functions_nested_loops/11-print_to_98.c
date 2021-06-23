84b7028ade33abaf7235f96fc4e3ebcb43c6cfd9
tihitna <tihitnasete58@gmail.com>
2021-06-23 01:22:59 -0700

    nested

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,28 @@ if (n <= 98)
 {
 for (i = n; i <= 98; i++)
 {
+if (i != 98)
+{
 printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
 }
 }
 else
 {
 for (i = n; i >= 98; i--)
 {
+if (i != 98)
+{
 printf("%d, ", i);
+}
+else
+{
+printf("%d", i);
+}
 }
 }
 printf("\n");
