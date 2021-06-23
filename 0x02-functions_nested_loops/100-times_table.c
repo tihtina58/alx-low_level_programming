5b8ab640da8f911fcfc2e1f1c25fc3d346648e70
tihitna <tihitnasete58@gmail.com>
2021-06-23 13:57:42 -0700

    nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,40 +5,40 @@
  */
 void print_times_table(int n)
 {
-int i, j, pro;
-if (n <= 15 && n >= 0))
+  int x, y, z;
+if (n <= 14 && n >= 0))
 {
-for (i = 0; i <= n; i++)
+for (x = 0; x <= n; x++)
 {
-for (j = 0; j <= n; j++)
+for (y = 0; y <= n; y++)
 {
-pro = i * j;
-if (j == 0)
+z = x * y;
+if (z > 99)
 {
-_putchar(48);
-continue;
-}
 _putchar(',');
-_putchar(' ');
-if (pro >= 10 && pro < 100)
-{
-_putchar(pro / 10 + 48);
-_putchar(pro % 10 + 48);
+_putchar(32);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');
+_putchar(z % 10) + '0');
 }
-else if (pro < 10 && x != 0)
+else if (z > 9)
 {
-_putchar((pro % 10) + 48);
-}
-else if (pro >= 100 && x != 0)
-{
-_putchar((pro / 100) + 48);
-_putchar((pro / 10) % 10 + 48);
-_putchar((pro % 10) + 48);
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
 }
 else
 {
-_putchar(' ');
-_putchar((pro % 10) + 48);
+if (y != 0)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
 }
 }
 _putchar('\n');
