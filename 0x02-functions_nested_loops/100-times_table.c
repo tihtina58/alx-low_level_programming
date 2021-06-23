367996fc21eacbd91722618f48cd6a53312ad704
tihitna <tihitnasete58@gmail.com>
2021-06-23 14:05:57 -0700

    nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  */
 void print_times_table(int n)
 {
-  int x, y, z;
+int x, y, z;
 if (n <= 14 && n >= 0)
 {
 for (x = 0; x <= n; x++)
@@ -19,7 +19,7 @@ _putchar(',');
 _putchar(32);
 _putchar((z / 100) + '0');
 _putchar(((z / 10) % 10) + '0');
- _putchar((z % 10) + '0');
+_putchar((z % 10) + '0');
 }
 else if (z > 9)
 {
