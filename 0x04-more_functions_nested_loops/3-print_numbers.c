a26df1a1452ceadad545b147910d14fe6d0fe231
tihitna <tihitnasete58@gmail.com>
2021-06-24 00:45:29 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers : print from 0 to 9
+ * Return: none
+ */
+void print_numbers(void)
+{
+char c;
+for (c = 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_number(void);
+
+#endif
