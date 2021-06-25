8280af0a1a04ade5514739c07ae3ec8f7affe76c
Tihitna <tihitnasete58@gmail.com>
2021-06-25 03:57:44 -0700

    fun

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,5 +2,9 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
