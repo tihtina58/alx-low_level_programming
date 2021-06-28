2d89f89bcf2edc85b0d0ef43a3755ed0cc11858a
root <tihtinasete58@gmail.com>
2021-06-28 08:31:56 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * puts2 -  prints every other character of a string
+ * @str: char parameter to be tested
+ * Return: none
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str += 2;
+}
+}
