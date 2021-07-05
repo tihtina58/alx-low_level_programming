93bbb8515a3ab6ba9ba6dfe8dfd820150a495816
Tihitna <tihitnasete58@gmail.com>
2021-07-05 11:42:12 -0700

    pointer and arrays

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,4 @@
+
 #include "holberton.h"
 /**
  *_memset -   fills memory with a constant byte
@@ -10,7 +11,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
-for (i = 0; n > 0; i++, n--)
+for (i = 0; n < 0; i++, n--)
 {
 s[i] = b; 
 }
