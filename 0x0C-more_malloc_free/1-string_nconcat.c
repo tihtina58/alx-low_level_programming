b05ea56daf9db4466b36396c1d26dae3aed42c7d
Tihitna <tihitnasete58@gmail.com>
2021-07-15 10:46:22 -0700

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,41 +2,40 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string to copy
- * @s2: second string to copy
- * @n: number of bytes of s2 to copy
+ * string_nconcat - concatenates two strings
+ * @s1: first string parameter
+ * @s2: second string parameter
+ * @n: size parameter
  *
- * Return: char pointer to newly allocated place in memory
+ * Return: concat pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  unsigned int i, j, k;
-  char *s;
+  char *concat;
+  unsigned int len = n, index;
 
   if (s1 == NULL)
-    i = 0;
-  else
-    {
-      for (i = 0; s1[i]; i++)
-	;
-    }
+    s1 = "";
+
   if (s2 == NULL)
-    j = 0;
-  else
-    {
-      for (j = 0; s2[j]; j++)
-	;
-    }
-  if (j > n)
-    j = n;
-  s = malloc(sizeof(char) * (i + j + 1));
-  if (s == NULL)
+    s2 = "";
+  for (index = 0; s1[index]; index++)
+    len++;
+
+  concat = (char *) malloc(sizeof(char) * (len + 1));
+
+  if (concat == NULL)
     return (NULL);
-  for (k = 0; k < i; k++)
-    s[k] = s1[k];
-  for (k = 0; k < j; k++)
-    s[k + i] = s2[k];
-  s[i + j] = '\0';
-  return (s);
+
+  len = 0;
+
+  for (index = 0; s1[index]; index++)
+    concat[len++] = s1[index];
+
+  for (index = 0; s2[index] && index < n; index++)
+    concat[len++] = s2[index];
+
+  concat[len] = '\0';
+
+  return (concat);
 }
