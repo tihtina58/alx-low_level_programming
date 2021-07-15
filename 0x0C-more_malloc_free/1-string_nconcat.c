fefe4c28df2e7d8f1b5df3870580d4de1a84cc2f
Tihitna <tihitnasete58@gmail.com>
2021-07-15 11:09:12 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,47 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * string_nconcat - Concatenates two strings using at
- *                  most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string.
+ * @s2: pointer to 2nd string.
+ * @n: Number of bytes from n2 to concatenate.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the concatenated space in memory.
+ * Return:Pointer to space in memory containing concatenated string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *concat;
-  unsigned int len = n, index;
+  char *str;
+  unsigned int i, j, s1_length, s2_length;
 
+  /*Check if the strings passed are null*/
   if (s1 == NULL)
     s1 = "";
-
   if (s2 == NULL)
     s2 = "";
+  /*Compute the length of the strings*/
 
-  for (index = 0; s1[index]; index++)
-    len++;
+  for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+    ;
 
-  concat = malloc(sizeof(char) * (len + 1));
+  for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+    ;
+  /*Memory reservation-for case 1 & 2.*/
+  str = malloc(s1_length + n + 1);
+  if (str == NULL)
+    {
+      return (NULL);
+    }
+  /*Copy first string into str.*/
+  for (i = 0; s1[i] != '\0'; i++)
+    str[i] = s1[i];
+  /*copy second stringg into str.*/
+  for (j = 0; j < n; j++)
+    {
+      str[i] = s2[j];
+      i++;
+    }
 
-  if (concat == NULL)
-    return (NULL);
-
-  len = 0;
-
-  for (index = 0; s1[index]; index++)
-    concat[len++] = s1[index];
-
-  for (index = 0; s2[index] && index < n; index++)
-    concat[len++] = s2[index];
-
-  concat[len] = '\0';
-
-  return (concat);
+  str[i] = '\0';
+  return (str);
 }
