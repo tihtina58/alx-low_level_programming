dc4858498c4bb5d41241319ef8b98c3a82921425
Tihitna <tihitnasete58@gmail.com>
2021-07-15 12:04:12 -0700

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,58 @@
-#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string to copy
- * @s2: second string to copy
- * @n: number of bytes of s2 to copy
- *
- * Return: char pointer to newly allocated place in memory
+ * _strlen - calculate length of a string
+ * @str: string
+ * Return: length of the string
  */
+
+int _strlen(char *str)
+{
+  int i;
+
+  for (i = 0; string[i] != '\0'; i++)
+    {
+    }
+  return (i);
+}
+
+/**
+ * string_nconcat - concatenate s1 with n char of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of chars to be concatinated
+ * Return: pointer to the concatenated string
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, k;
-char *s;
-if (s1 == NULL)
-i = 0;
-else
-{
-for (i = 0; s1[i]; i++)
-;
-}
-if (s2 == NULL)
-j = 0;
-else
-{
-for (j = 0; s2[j]; j++)
-;
-}
-if (j > n)
-j = n;
-s = malloc(sizeof(char) * (i + j + 1));
-if (s == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-s[k] = s1[k];
-for (k = 0; k < j; k++)
-s[k + i] = s2[k];
-s[i + j] = '\0';
-return (s);
+  char *p;
+  int nn, len, i, j;
+
+  nn = n;
+
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)
+    s2 = "";
+  if (nn < 0)
+    return (NULL);
+  if (nn >= _strlen(s2))
+    nn = _strlen(s2);
+
+  len = _strlen(s1) + nn + 1;
+
+  p = malloc(sizeof(*p) * len);
+  if (p == NULL)
+    return (NULL);
+
+  for (i = 0; s1[i] != '\0'; i++)
+    p[i] = s1[i];
+  for (j = 0; j < nn; j++)
+    p[i + j] = s2[j];
+  p[i + j] = '\0';
+
+  return (p);
 }
