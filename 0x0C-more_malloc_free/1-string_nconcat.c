18d6475dd2febd3ecdaf720d93be9b2de02cfb11
Tihitna <tihitnasete58@gmail.com>
2021-07-15 11:23:23 -0700

    k

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,39 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of bytes from s2 to be copied
- * Return: concatenated string
+ *@s1: string one
+ *@s2: string two
+ *@n: number of bytes
+ * Return: string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  unsigned int i, j, size, len1, len2;
-  char *ptr;
+  char *a;
+  unsigned int i, j, str1, str2;
 
   if (s1 == NULL)
     s1 = "";
   if (s2 == NULL)
     s2 = "";
-  len1 = _strlen(s1);
-  len2 = _strlen(s2);
-  if (n > len2)
-    n = len2;
-  size = len1 + n;
-  ptr = malloc(sizeof(char) * size + 1);
-  if (!ptr)
+  for (i = 0; s1[i] != '\0'; i++)
+    ;
+  for (j = 0; s2[j] != '\0' && j < n; j++)
+    ;
+  str1 = i;
+  str2 = j;
+  a = malloc(sizeof(char) * (str1 + str2 + 1));
+  if (a == NULL)
     return (NULL);
-  for (i = 0; i < len1; i++)
-    ptr[i] = s1[i];
-  for (j = 0; j < n; j++, i++)
-    ptr[i] = s2[j];
-  ptr[i] = 0;
-  return (ptr);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-  char *p = s;
-
-  while (*s)
-    s++;
- return (s - p);
+  for (i = 0; i < str1; i++)
+    {
+      a[i] = s1[i];
+    }
+  for (j = 0; j < str2; j++)
+    a[i + j] = s2[j];
+  a[i + j] = '\0';
+  return (a);
 }
