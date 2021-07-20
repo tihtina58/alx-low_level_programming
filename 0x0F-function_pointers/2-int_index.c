4f146fc488299662cb0d635f698f2d021db9ff47
Tihitna <tihitnasete58@gmail.com>
2021-07-20 11:11:43 -0700

    function pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - xecutes a function given as a parameter on each
+ * element of an array.
+ * @array: array of intege
+ * @size: size of array
+ * @action: function pointer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+  int i = 0;
+  if (size <= 0)
+    return (-1);
+      while (i < size)
+	{
+	  if (cmp(array[i]))
+	    return (i);
+	  i++;
+	}
+      return (-1);  
+}
